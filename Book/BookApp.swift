61ac1c57c55512bfb7c1c8857bba4798580a2b41
stepanokdev <stepanokdev@gmail.com>
2023-04-22 11:32:54 +0300

    Initial Commit

---
+++ Book/BookApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookApp.swift
+//  Book
+//
+//  Created by  Stepanok Ivan on 22.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct BookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
